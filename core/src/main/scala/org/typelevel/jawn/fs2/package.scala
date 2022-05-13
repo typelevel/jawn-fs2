216892a51d8eac0885867501e3f9e47e875c76a0
Scala Steward <me@scala-steward.org>
2022-05-13 19:34:00 +0200

    Reformat with scalafmt 3.5.3

--- core/src/main/scala/org/typelevel/jawn/fs2/package.scala
+++ core/src/main/scala/org/typelevel/jawn/fs2/package.scala
@@ -42,7 +42,7 @@ package object fs2 { self =>
         attempt.fold(Pull.raiseError[F], js => Pull.output(Chunk.seq(js)))
 
       s.pull.uncons1.flatMap {
-        case Some((a, stream)) =>
+        case Some(a, stream) =>
           handle(A.absorb(parser, a)) >> go(parser)(stream)
         case None =>
           handle(parser.finish()) >> Pull.done
